56d6142cb69f56a901df58e3fd444467eaaec1ee
FDULeolu <fdulyz@sina.com>
2023-01-07 22:39:27 +0800

    删去重复代码

--- Accelerated C++/Chapter 3/计算学生成绩.cpp
+++ Accelerated C++/Chapter 3/计算学生成绩.cpp
@@ -47,39 +47,5 @@ int main(){
 
     return 0;
 
-    /*
-    //向量解法计算中位数
-    double x;
-    std::vector<double> homework;
-
-    while (cin >> x)
-    {
-        homework.push_back(x);    
-    }
-
-    typedef vector<double>::size_type vec_sz;
-    vec_sz size = homework.size();
-
-    if (size == 0){
-        cout << endl << "You must enter your grades."
-                     << "Please try again." << endl;
-        return 1;
-    }
-
-    sort(homeworl.begin(), homework.end());
-
-    vec_sz mid = size/2;
-    double median;
-    median = (size % 2 == 0) ? (homework[mid] + homework[mid-1]) / 2 : homework[mid];
-
-    //输出结果
-    streamsize prec = cout.precision();
-    cout << "Your final grade is " << setprecision(3)
-         << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
-         << setprecision(prec) << endl;
-
-    return 0;
-    */
-    
 }
 
